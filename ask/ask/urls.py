440b8270ff589718359c33eff3ce25683b324025
pteacher <subfear@mail.ru>
2016-03-07 13:49:05 +0000

    good

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -3,10 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ask.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url)r'^', include(aq.urls)),
+urlpatterns = [
+    url(r'^$', 'qa.views.test'),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
