ce325d6b8637fd735622ad6dde7b32bcd1349548
crazydiv <crazydiv@yandex.ru>
2016-03-09 20:20:59 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
+pidfile = "/home/box/web/gunicorn/gunicorn.pid"
+accesslog = "/home/box/web/gunicorn/access.log"
+errorlog = "/home/box/web/gunicorn/error.log"
+pythonpath = "/home/box/web/ask/ask"
+workers = 16
+timeout = 60
+
 def wsgi_app(env, start_responce):
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
